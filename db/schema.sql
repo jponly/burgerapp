7301e85ba3744ecce1c72dad3eca04093ed61a52
jponly <jpangeli514@gmail.com>
2018-10-01 22:02:28 -0700

    VersionX

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devour BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name, devour) VALUES ('Pierce Brosnan Burger', true);
+INSERT INTO burger (name) VALUES ('Swiss Cheddar');
+INSERT INTO burger (name) VALUES ('Ultimate Bacon');
+INSERT INTO burger (name, devour) VALUES ('Southwest Onion Ring', true);
+INSERT INTO burger (name) VALUES ('Jethro Burger');
+INSERT INTO burger (name, devour) VALUES ('Whiplash Burger', true);
